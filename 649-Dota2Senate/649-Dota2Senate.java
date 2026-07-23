8c05fb1419ea5ad902546f729483a86ca627421a
Mihir Bansal <mibansal23@gmail.com>
2026-07-23 09:33:56 +0530

    Create 649-Dota2Senate

---
+++ 649-Dota2Senate/649-Dota2Senate.java
@@ -0,0 +1,30 @@
+// Last updated: 7/23/2026, 9:33:56 AM
+class Solution {
+    public String predictPartyVictory(String senate) {
+        int n = senate.length();
+        Queue<Integer> R = new LinkedList<>();
+        Queue<Integer> D = new LinkedList<>();
+        
+        for(int i = 0; i < n; i++){
+            if(senate.charAt(i) == 'R'){
+                R.offer(i);
+            } else{
+                D.offer(i);
+            }
+        }
+        while(!R.isEmpty() && !D.isEmpty()){
+            int r = R.poll();
+            int d = D.poll();
+            if(r < d){
+                R.offer(r + n);
+            } else {
+                D.offer(d + n);
+            }
+        }
+        if(R.isEmpty()){
+            return "Dire";
+        } else{
+            return "Radiant";
+        }
+    }
+}
