3d0d41c4ed7b0083f0f08f265b84d97f339efc4b
Mihir Bansal <mibansal23@gmail.com>
2026-07-23 09:33:21 +0530

    Create 1212-SequentialDigits

---
+++ 1212-SequentialDigits/1212-SequentialDigits.java
@@ -0,0 +1,17 @@
+// Last updated: 7/23/2026, 9:33:21 AM
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> res = new ArrayList<>();
+        for(int i = 1; i <= 9; i++){
+            int num = i;
+            for(int j = i+1; j <= 9; j++){
+                num = num * 10+j;
+                if(num >= low && num <= high){
+                    res.add(num);
+                }
+            }
+        }
+        Collections.sort(res);
+        return res;
+    }
+}
