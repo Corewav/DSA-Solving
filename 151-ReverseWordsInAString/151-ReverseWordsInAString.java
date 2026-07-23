1ff0378e6f4629861f52f08e72af0e799c1818c0
Mihir Bansal <mibansal23@gmail.com>
2026-07-23 09:35:17 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.java
@@ -0,0 +1,22 @@
+// Last updated: 7/23/2026, 9:35:16 AM
+class Solution {
+    public String reverseWords(String s) {
+        String result = new String();
+        int i = 0;
+        int n = s.length();
+        while(i < n){
+            while(i < n && s.charAt(i) == ' ')
+            i++;
+            if(i>=n)break;
+            int j=i+1;
+            while(j<n && s.charAt(j)!= ' ')
+            j++;
+            String sub = s.substring(i,j);
+            if(result.length()==0)
+            result  = sub;
+            else result= sub + " "+ result;
+            i=j+1;
+        }
+        return result;
+    }
+}
