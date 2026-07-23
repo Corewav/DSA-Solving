d2991f1223c8f8a719f1964f215dec211e9056c5
Mihir Bansal <mibansal23@gmail.com>
2026-07-23 09:33:29 +0530

    Create 1021-DistributeCoinsInBinaryTree

---
+++ 1021-DistributeCoinsInBinaryTree/1021-DistributeCoinsInBinaryTree.java
@@ -0,0 +1,30 @@
+// Last updated: 7/23/2026, 9:33:29 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int move =0;
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return move;
+    }
+    private int dfs (TreeNode node){
+        if(node == null) return 0;
+        int left  = dfs(node.left);
+        int right = dfs(node.right);
+        move += Math.abs(left) + Math.abs(right);
+        return node.val -1 + right + left;
+    }
+}
