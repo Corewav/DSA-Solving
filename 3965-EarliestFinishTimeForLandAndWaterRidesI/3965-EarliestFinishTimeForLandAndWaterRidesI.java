943964885e445095d4e9505d6efc90d84a81b8a0
Mihir Bansal <mibansal23@gmail.com>
2026-07-23 09:31:57 +0530

    Create 3965-EarliestFinishTimeForLandAndWaterRidesI

---
+++ 3965-EarliestFinishTimeForLandAndWaterRidesI/3965-EarliestFinishTimeForLandAndWaterRidesI.java
@@ -0,0 +1,21 @@
+// Last updated: 7/23/2026, 9:31:57 AM
+class Solution {
+    public int earliestFinishTime(int[] lst, int[] ldu, int[] wst, int[] wdu) {
+        int ans = Integer.MAX_VALUE;
+        int mln = Integer.MAX_VALUE;
+        int mnw = Integer.MAX_VALUE;
+        for(int i = 0; i <lst.length;i++){
+            mln = Math.min(mln,lst[i] + ldu[i]);
+        }
+        for(int i =0; i< wst.length;i++){
+            ans = Math.min(ans, Math.max(mln,wst[i]) + wdu[i]);
+        }
+        for(int i =0; i< wst.length; i++){
+            mnw = Math.min(mnw, wst[i] + wdu[i]);
+        }
+        for(int i = 0; i<lst.length;i++){
+            ans = Math.min(ans, Math.max(mnw, lst[i]) + ldu[i]);
+        }
+        return ans;
+    }
+}
