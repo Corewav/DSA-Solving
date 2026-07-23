953910fe3ecfe64ca327dad8b84b2dfbe836d49c
Mihir Bansal <mibansal23@gmail.com>
2026-07-23 09:33:14 +0530

    Create 1460-NumberOfSubstringsContainingAllThreeCharacters

---
+++ 1460-NumberOfSubstringsContainingAllThreeCharacters/1460-NumberOfSubstringsContainingAllThreeCharacters.java
@@ -0,0 +1,12 @@
+// Last updated: 7/23/2026, 9:33:14 AM
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int [] lastChar = {-1,-1,-1};
+        int ans = 0;
+        for(int i = 0;i<s.length(); i++){
+            lastChar[s.charAt(i)- 'a'] = i;
+            ans += 1+ Math.min(lastChar[0], Math.min(lastChar[1],lastChar[2]));
+        }
+        return ans;
+    }
+}
