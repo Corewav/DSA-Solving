71424760cc6696106d9ce000ce73ad3003ada206
Mihir Bansal <mibansal23@gmail.com>
2026-07-23 09:35:48 +0530

    Create 105-ConstructBinaryTreeFromPreorderAndInorderTraversal

---
+++ 105-ConstructBinaryTreeFromPreorderAndInorderTraversal/105-ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,77 @@
+// Last updated: 7/23/2026, 9:35:48 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     HashTree<Integer,Integer> map = new HashMap<>();
+//     int preIndex =0;
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+//         for(int i=0;i<inorder.length;i++){
+//             {
+//                 map.put(inorder[i],i);
+//             }
+//             return helper(preorder,0, inorder.length-1);
+//         }
+//         private TreeNode helper(int[] preorder, int left, int right){
+//             if(left>right){
+//                 return null;
+//             }
+//             int rootVal = preorder[preIndex++];
+//             TreeNode root = new TreeNode(rootVal);
+//             int index = map.get(rootVal);
+            
+//             root.left = helper(preorder, left, index -1);
+//             root.right = helper(preorder, index +1, right);
+//             return root;
+        
+
+        
+        
+        
+//         }
+// }
+
+
+class Solution {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int preIndex = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        for(int i = 0; i < inorder.length; i++){
+            map.put(inorder[i], i);
+        }
+
+        return helper(preorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode helper(int[] preorder, int left, int right){
+
+        if(left > right){
+            return null;
+        }
+
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int index = map.get(rootVal);
+
+        root.left = helper(preorder, left, index - 1);
+        root.right = helper(preorder, index + 1, right);
+
+        return root;
+    }
+}
