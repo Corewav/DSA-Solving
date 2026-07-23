e916a0132432556dcf400c654b1d578bff2193c8
Mihir Bansal <mibansal23@gmail.com>
2026-07-23 09:32:12 +0530

    Create 3329-FindTheLengthOfTheLongestCommonPrefix

---
+++ 3329-FindTheLengthOfTheLongestCommonPrefix/3329-FindTheLengthOfTheLongestCommonPrefix.java
@@ -0,0 +1,22 @@
+// Last updated: 7/23/2026, 9:32:12 AM
+class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        HashSet<Integer> st = new HashSet<>();
+        for(int el : arr1){
+            while(!st.contains(el) && el > 0){
+                st.add(el);
+                el /= 10;
+            }
+        }
+        int ans =0;
+        for(int el : arr2){
+            while(!st.contains(el) && el > 0){
+                el /= 10;
+            }
+            if(el > 0){
+                ans = Math.max(ans,(int)(Math.log10(el))+1);
+            }
+        }
+        return ans;
+    }
+}
