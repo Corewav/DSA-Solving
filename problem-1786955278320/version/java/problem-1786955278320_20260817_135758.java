4f7a850b56f7624f6d3c0aac20afaa36fef60058
Mihir Bansal <mibansal23@gmail.com>
2026-08-17 13:57:59 +0530

    Create

---
+++ problem-1786955278320/version/java/problem-1786955278320_20260817_135758.java
@@ -0,0 +1,10 @@
+// Last updated: 8/17/2026, 1:57:58 PM
+1class Solution {
+2    public boolean canWinNim(int n) {
+3        if(n%4==0){
+4            return false;
+5        }else{
+6            return true;
+7        }
+8    }
+9}
