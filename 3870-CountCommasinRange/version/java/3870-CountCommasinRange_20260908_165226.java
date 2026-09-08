0fadcdee9a2dd4401422977502f5437eb94fc0d4
Mihir Bansal <mibansal23@gmail.com>
2026-09-08 16:52:27 +0530

    Create 3870-CountCommasinRange

---
+++ 3870-CountCommasinRange/version/java/3870-CountCommasinRange_20260908_165226.java
@@ -0,0 +1,9 @@
+// Last updated: 9/8/2026, 4:52:26 PM
+1class Solution {
+2    public int countCommas(int n) {
+3        if(n<1000){
+4            return 0;
+5        }
+6        return n-999;
+7    }
+8}
