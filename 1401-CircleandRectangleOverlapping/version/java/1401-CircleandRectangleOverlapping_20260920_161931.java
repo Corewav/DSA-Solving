c07221f23997beb35afbaddabd0fd4c9dd1f6ea6
Mihir Bansal <mibansal23@gmail.com>
2026-09-20 16:19:32 +0530

    Create 1401-CircleandRectangleOverlapping

---
+++ 1401-CircleandRectangleOverlapping/version/java/1401-CircleandRectangleOverlapping_20260920_161931.java
@@ -0,0 +1,11 @@
+// Last updated: 9/20/2026, 4:19:31 PM
+1class Solution {
+2    public boolean checkOverlap(int r, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+3        int nx=Math.max(x1,Math.min(x2,xCenter));
+4        int ny=Math.max(y1,Math.min(y2,yCenter));
+5        int dx=xCenter-nx;
+6        int dy=yCenter-ny;
+7        return (dx*dx+dy*dy)<=r*r;
+8
+9    }
+10}
